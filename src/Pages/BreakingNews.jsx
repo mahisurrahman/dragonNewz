63d295e9c696bb7c6c01670a39e5b9cdba1f92c9
mahisurrahman <zordanblog009@gmail.com>
2023-10-06 01:22:04 +0600

    Header Section completed

---
+++ src/Pages/BreakingNews.jsx
@@ -0,0 +1,15 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+const BreakingNews = () => {
+    return (
+        <div className="flex bg-slate-200 px-4 py-4 rounded-lg">
+            <button className="btn btn-error rounded-none ml-4 mr-1 px-6 py-2">Latest</button>
+            <Marquee className="font-bold" pauseOnHover={true} speed={100}>
+            <Link to="/">Match Highlights: Germany vs Spain — as it happened ! Match Highlights: Germany vs Spain as...</Link>
+            </Marquee>
+        </div>
+    );
+};
+
+export default BreakingNews;
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,8 +1,14 @@
+import logo from '../../../assets/assets/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h2 className="text-3xl">Header</h2>
+            <div className='text-center mt-10'>
+                <img className='mx-auto' src={logo} alt="site_logo" />
+                <p className='text-xl font-light mt-5'>Journalism Without Fear or Favour</p>
+                <p className='text-2xl font-medium mt-2'>{moment().format('LLLL')}</p>
+            </div>
         </div>
     );
 };
