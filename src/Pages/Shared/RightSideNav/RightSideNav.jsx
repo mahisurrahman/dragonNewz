eb6e5922e640445a15b1f7133a28889758ae60ea
mahisurrahman <zordanblog009@gmail.com>
2023-10-06 03:43:50 +0600

    Right Side Menu Completed

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -4,11 +4,15 @@ import { FaFacebook } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
 
+import qZoneOne from '../../../assets/assets/qZone1.png';
+import qZoneTwo from '../../../assets/assets/qZone2.png';
+import qZoneThree from '../../../assets/assets/qZone3.png';
+
 const RightSideNav = () => {
     return (
         <div>
-            <div className='p-4 space-y-3 mb-2'>
-                <h2 className="text-xl font-semibold pl-4 py-4">Login with</h2>
+            <div className='pl-4 pr-4 pb-4 mb-2'>
+                <h2 className="text-xl font-semibold pl-4 pb-4">Login with</h2>
                 <button className="btn w-full border-2 border-blue-500 text-blue-500 bg-white">
                     <FaGoogle></FaGoogle>
                     Sign-In with Google
@@ -18,11 +22,19 @@ const RightSideNav = () => {
                     Sign-In with Github
                 </button>
             </div>
-            <div className='p-4 mb-2 border rounded-lg mt-5'>
+            <div className='p-4 mt-5'>
                 <h2 className="text-xl font-semibold pl-4 py-4">Find Us On</h2>
-                <a href="" className='p-4 flex items-center'><FaFacebook className='mx-2 w-7 h-7'></FaFacebook> Facebook</a>
-                <a href="" className='p-4 flex items-center'><FaTwitter className='mx-2 w-7 h-7'></FaTwitter> Twitter</a>
-                <a href="" className='p-4 flex items-center'><FaInstagram className='mx-2 w-7 h-7'></FaInstagram> Instagram</a>
+                <a href="" className='p-4 flex items-center border rounded-t-lg mb-2 hover:bg-gray-300'><FaFacebook className='mx-2 w-7 h-7'></FaFacebook> Facebook</a>
+                <a href="" className='p-4 flex items-center border mb-2 hover:bg-gray-300'><FaTwitter className='mx-2 w-7 h-7'></FaTwitter> Twitter</a>
+                <a href="" className='p-4 flex items-center border rounded-b-lg mb-2 hover:bg-gray-300'><FaInstagram className='mx-2 w-7 h-7'></FaInstagram> Instagram</a>
+            </div>
+            <div className='p-4 mb-2'>
+                <div className=' bg-slate-100 py-5 px-1 rounded'>
+                <h2 className="text-2xl font-semibold pl-4 pb-4 mb-2">Q Zone</h2>
+                <img className="my-6" src={qZoneOne} />
+                <img className="my-6" src={qZoneTwo} />
+                <img className="my-6" src={qZoneThree} />
+                </div>
             </div>
         </div>
     );
